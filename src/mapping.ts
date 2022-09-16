0f7052625dbd48244e606b9f9730c51bbe35c85e
ad <adrianleb@gmail.com>
2022-09-16 16:12:32 +0200

    init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,15 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import {RegisterPoolCall} from '../generated/SudoGate/SudoGate';
+import { Actor, NFTContract, SudoPool } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleRegisterPool(call: RegisterPoolCall): void {
+  // let caller = call.transaction.from;
+  // let actor = Actor.load(caller.toHexString());
+
+  // if (!actor) {
+  //   actor = new Actor(caller.toHexString());
+  // }
+
+  let pool = new SudoPool(call.inputs.sudoswapPool.toHexString())
+  pool.save()
 }
